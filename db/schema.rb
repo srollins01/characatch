4f76d065b079e9c958c53315f824038705e842db
Danielle Matsumoto <danielle.a.smart@gmail.com>
2023-01-28 12:42:17 +0900

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_025644) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2023_01_28_030145) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
