b5520df5b3294a3c2e207787177a16767cef97ad
Sarah Rollins <s.rollins751@gmail.com>
2023-01-28 11:44:24 +0900

    added rating to review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_115014) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_28_024105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_115014) do
     t.datetime "updated_at", null: false
     t.bigint "listing_id", null: false
     t.bigint "user_id", null: false
+    t.integer "rating"
     t.index ["listing_id"], name: "index_reviews_on_listing_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
