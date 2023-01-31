71cbb1eae17fe62076872bc0c7fd1c22f7273f8e
Tanner Maxwell <tanner.ray.maxwell@gmail.com>
2023-01-31 21:37:02 +0900

    made booking_card partial

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,9 +22,9 @@ class BookingsController < ApplicationController
   def update
     @booking = Booking.find(params[:id])
     if @booking.update(booking_params)
-      redirect_to dashboard_path
+      redirect_to dashboard_path(@listing)
     else
-
+      render :new, status: :unprocessable_entity
     end
   end
 
