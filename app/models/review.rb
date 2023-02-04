a7b55ec5ad56cf0bff71757545d07ba05e928399
Tanner Maxwell <tanner.ray.maxwell@gmail.com>
2023-02-04 13:11:26 +0900

    booking card changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :listing
   belongs_to :user
-
   validates :content, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { greater_than: 0, less_than: 6 }
 end
