a837c32f93903fab61467b4c3035ce854c5e14a3
Tanner Maxwell <tanner.ray.maxwell@gmail.com>
2023-02-04 14:56:15 +0900

    updated seed file again

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :listing
-  belongs_to :review
   enum status: %i[pending accepted declined completed]
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :listing
   belongs_to :user
-  belongs_to :booking
   validates :content, presence: true
   validates :rating, presence: true, numericality: { greater_than: 0, less_than: 6 }
 end
